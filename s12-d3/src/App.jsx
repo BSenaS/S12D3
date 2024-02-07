8086f67a8e720bc5f86457a9eea04d4df03fe457
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-02-07 11:21:38 +0300

    Axios , useEffect first fetch.

--- s12-d3/src/App.jsx
+++ s12-d3/src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [deger, setDeger] = useState({});
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setDeger(json));
+  }, []);
+  console.log(deger);
+  return (
+    <>
+      <div>
+        <h2>{deger.title}</h2>
+      </div>
+    </>
+  );
 }
 
 export default App;
