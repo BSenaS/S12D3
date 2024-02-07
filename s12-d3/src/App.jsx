ee9640800410edcdf290a12cbe5f2f1858e0b949
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-02-07 12:45:06 +0300

    axios closed #5 #6 #7

--- s12-d3/src/App.jsx
+++ s12-d3/src/App.jsx
@@ -1,20 +1,52 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+// const baseURL = "https://fakestoreapi.com/products";
 
 function App() {
-  const [deger, setDeger] = useState({});
+  const [products, setProducts] = useState([]);
+
+  const instance = axios.create({
+    baseURL: "https://fakestoreapi.com/products",
+    timeout: 1000,
+    headers: { "Secret-Custom-Header": "token" },
+  });
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
-      .then((json) => setDeger(json));
+    instance
+      .get()
+      .then(function (response) {
+        // handle successsetProducts(json)
+        setProducts(response.data);
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
   }, []);
-  console.log(deger);
+
+  console.log(products);
+  if (products.length === 0) {
+    return <div>Loading...</div>;
+  }
   return (
-    <>
-      <div>
-        <h2>{deger.title}</h2>
-      </div>
-    </>
+    <div className="App">
+      {products.map((product) => (
+        <div key={product.id} className="product">
+          <img src={product.image} alt={product.title} />
+          <div>
+            <h2>{product.title}</h2>
+            <p>Price: ${product.price}</p>
+            <p>Description: {product.description}</p>
+            <p>Category: {product.category}</p>
+            <p>
+              Rating: {product.rating.rate} ({product.rating.count} reviews)
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
